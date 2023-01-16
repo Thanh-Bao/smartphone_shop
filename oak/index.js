7a49c8b9eafd4a89cec87e710717246977669e89
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2023-01-17 01:36:13 +0700

    .

--- oak/index.js
+++ oak/index.js
@@ -5,23 +5,50 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { encode, decode } from "https://deno.land/std/encoding/base64.ts"
+import { Client } from "https://deno.land/x/mysql/mod.ts";
 
-import { access_token, refresh_toke, accountAuthen, authenURL, getNewAccessTokenURL, baseURL, setAccess_token, setRefresh_token, MySQL_config } from './config.js';
+import { access_token, refresh_token, accountAuthen, authenURL, getNewAccessTokenURL, baseURL, setAccess_token, setRefresh_token, MySQL_config } from './config.js';
 
 const router = new Router();
 
+const MySQL_client = await new Client().connect(MySQL_config);
 
+
+const getNewAccessToken = async () => {
+  const basic_auth = encode(`${accountAuthen.username}:${accountAuthen.password}`);
+  const SAPresponse = await fetch(getNewAccessTokenURL, {
+    headers: { "Authorization": `Basic ${basic_auth}` }
+  });
+  const json = await SAPresponse.json();
+  setAccess_token(json.access_token);
+  setRefresh_token(json.refresh_token);
+}
+
+getNewAccessToken();
+
+// For Cronb Job servie: /renew_access_token?isCronJob=true
 router.get("/renew_access_token", async (ctx) => {
 
+  const isCronJob = Boolean(ctx.request.url.searchParams.get('isCronJob'));
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+
+  //🤩🤩 Imperative Programming : 🤩🤩
   try {
-    const basic_auth = encode(`${accountAuthen.username}:${accountAuthen.password}`);
-    const SAPresponse = await fetch(getNewAccessTokenURL, {
-      headers: { "Authorization": `Basic ${basic_auth}` }
-    });
-    const json = await SAPresponse.json();
-    setAccess_token(json.access_token);
-    ctx.response.body = json;
+    getNewAccessToken();
+    const JWT = json.access_token.split(".");
+    const JWT_payload = JSON.parse(new TextDecoder().decode(decode(JWT[1])));
+    const JWT_expired = JWT_payload.exp;
+    const SQL_result = await MySQL_client.execute(`INSERT INTO Token_Log(timestamp,	Access_Token,Refresh_Token,	Expired_time,isSuccess,isCronJob,Error_message) values(?,?,?,?,?,?,?)`, [
+      currentTimestamp, access_token, refresh_token, JWT_expired, true, isCronJob, null
+    ]);
+    console.log(SQL_result);
+    // 4. Finish
+    ctx.response.body = { ...json, __________token_after_decode__________: JWT_payload };
   } catch (error) {
+    const SQL_result = await MySQL_client.execute(`INSERT INTO Token_Log(timestamp,	Access_Token,Refresh_Token,	Expired_time,isSuccess,isCronJob,Error_message) values(?,?,?,?,?,?,?)`, [
+      currentTimestamp, null, null, null, false, isCronJob, error.message
+    ]);
+    console.log(SQL_result);
     ctx.response.body = error.message;
     ctx.response.status = 401;
   }
@@ -57,11 +84,10 @@ router.get("/", (ctx) => {
     }],
     site_map: {
       new_token_manual: "/renew_access_token",
+      CronJob_renew_token: "/renew_access_token?isCronJob=true"
     }
   };
 });
-
-
 
 
 const app = new Application();
