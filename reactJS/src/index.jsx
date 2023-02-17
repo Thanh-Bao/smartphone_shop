7136f5fd72c417bcd18c4b8236f8287727bc8b8e
Bao Bao <thanh.bao@outlook.com>
2023-02-17 09:45:22 +0700

    add navigation bar + card

--- reactJS/src/index.jsx
+++ reactJS/src/index.jsx
@@ -6,6 +6,12 @@ import "./index.css";
 
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme";
 import "@ui5/webcomponents-react/dist/Assets";
+import "@ui5/webcomponents-fiori/dist/Assets.js";
+import "@ui5/webcomponents-icons/dist/cart";
+import "@ui5/webcomponents-icons/dist/heart-2";
+import "@ui5/webcomponents-icons/dist/nutrition-activity";
+import "@ui5/webcomponents-icons/dist/paid-leave";
+import "@ui5/webcomponents-icons/dist/shipping-status";
 
 import Home from "./pages/Home";
 import PhoneDetail from "./pages/PhoneDetail";
--- reactJS/src/pages/Home.jsx
+++ reactJS/src/pages/Home.jsx
@@ -5,7 +5,16 @@ import {
   Avatar,
   Input,
   Icon,
+  Carousel,
+  Card,
+  CardHeader,
+  List,
+  Button,
+  Badge,
+  MediaGalleryItem,
+  Title,
 } from "@ui5/webcomponents-react";
+import REGEX from "regex-awesome";
 
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
 setTheme("sap_belize_hcb");
@@ -53,6 +62,109 @@ const Home = () => {
         style={{}}
         waitForDefine
       />
+      <hr />
+      <Carousel
+        style={{ height: 500 }}
+        cyclic
+        itemsPerPageL={1}
+        onNavigate={function noRefCheck() {}}
+      >
+        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_1920x533/https://cdn.tgdd.vn/2023/02/banner/iPad10-2880-800-1920x533-1.png" />
+        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_1920x533/https://cdn.tgdd.vn/2023/02/banner/Airpods-Pro-2880-800-1920x533-1.png" />
+        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_1920x533/https://cdn.tgdd.vn/2023/02/banner/Mac-2880-800-1920x533.png" />
+        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_1920x533/https://cdn.tgdd.vn/2023/02/banner/AW8-2880-800-1920x533.png" />
+      </Carousel>
+      <div className="mx-4 ml-6 mb-6 flex flex-wrap">
+        {[...Array(10).keys()].map((item) => {
+          return (
+            <div className="m-8">
+              <Card
+                accessibleName=""
+                accessibleNameRef=""
+                className=""
+                header={
+                  <CardHeader
+                    avatar={<Icon name="nutrition-activity" />}
+                    status="còn hàng"
+                    subtitleText="Bảo hành 36 tháng"
+                    titleText="Tặng củ sạc"
+                  />
+                }
+                style={{
+                  width: "300px",
+                }}
+                waitForDefine
+              >
+                <List>
+                  <StandardListItem className="py-36">
+                    <div>
+                      <MediaGalleryItem>
+                        <img src="https://sap.github.io/ui5-webcomponents/main/assets/images/HT-1022.jpg" />
+                      </MediaGalleryItem>
+                    </div>
+                  </StandardListItem>
+                  <StandardListItem>
+                    <div className="flex">
+                      <Badge
+                        colorScheme="5"
+                        icon={<Icon name="shipping-status" />}
+                      >
+                        Free ship
+                      </Badge>
+                      <Badge
+                        className="ml-3"
+                        colorScheme="8"
+                        icon={<Icon name="paid-leave" />}
+                      >
+                        Trả góp 0%
+                      </Badge>
+                    </div>
+                  </StandardListItem>
+                  <StandardListItem>
+                    <Title level="H4">Iphone 18 Pro Max ultra</Title>
+                  </StandardListItem>
+                  <StandardListItem>
+                    <div className="flex justify-center items-center">
+                      <Title className="text-red-500" level="H3">
+                        {REGEX.dotDecimal(23990000)} đ
+                      </Title>
+                      <Badge className="ml-2" colorScheme="2">
+                        -20%
+                      </Badge>
+                    </div>
+                  </StandardListItem>
+                  <StandardListItem>
+                    <div className="flex justify-center items-center">
+                      <span className="line-through text-slate-400 text-lg">
+                        {REGEX.dotDecimal(33990000)} đ
+                      </span>
+                    </div>
+                  </StandardListItem>
+                  <StandardListItem className="py-8">
+                    <div className="flex justify-between">
+                      <Button
+                        design="Emphasized"
+                        icon="cart"
+                        onClick={function noRefCheck() {}}
+                        tooltip="Thêm vào giỏ hàng"
+                      >
+                        Add cart
+                      </Button>
+                      <Button
+                        icon="heart-2"
+                        onClick={function noRefCheck() {}}
+                        tooltip="Thêm vào danh sách yêu thích"
+                      >
+                        Add wishlist
+                      </Button>
+                    </div>
+                  </StandardListItem>
+                </List>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
